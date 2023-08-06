aaf10927c1725874243d5a1f5c3ff4ca31e5326d
meetorion <leejoy@mail.ustc.edu.cn>
2023-08-06 21:00:36 +0800

    modify obsidina dir

--- lua/orion/obsidian.lua
+++ lua/orion/obsidian.lua
@@ -1,7 +1,7 @@
 require("obsidian").setup({
     {
         -- Required, the path to your vault directory.
-        dir = "~/obsidian/neovim",
+        dir = "~/notespace/zk",
 
         -- Optional, if you keep notes in a specific subdirectory of your vault.
         notes_subdir = "notes",
@@ -113,3 +113,27 @@ require("obsidian").setup({
         open_notes_in = "current"
     }
 })
+
+local M = {
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
+}
+
+M.config = function()
+    local project = require("project_nvim")
+    project.setup({
+        active = true,
+        on_config_done = nil,
+        manual_mode = false,
+        detection_methods = { "pattern" },
+        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".project_root" },
+        show_hidden = false,
+        silent_chdir = true,
+        ignore_lsp = {},
+    })
+
+    local telescope = require("telescope")
+    telescope.load_extension("projects")
+end
+
+return M
